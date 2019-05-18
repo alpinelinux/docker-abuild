3766ea7fd81a64b1bce5d8d73ba34c685e41acb0
Carlo Landmeter <clandmeter@alpinelinux.org>
2019-05-18 10:53:48 +0000

    entrypoint: update indexes and world

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,5 +16,6 @@ fi
 ( . "$HOME/.abuild/abuild.conf" ; if [ ! -s "$PACKAGER_PRIVKEY" ]; then abuild-keygen -n -a; fi )
 
 sudo cp -v "$HOME"/.abuild/*.rsa.pub /etc/apk/keys/
+sudo apk -U upgrade -a
 
 exec "$(command -v abuild)" "$@"
