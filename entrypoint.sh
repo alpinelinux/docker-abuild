f13d7adcc61109fb3cfceba9a36799a41cf7e40a
Richard Mortier <mort@cantab.net>
2019-09-07 23:04:55 +0100

    whitespace
    Signed-off-by: Richard Mortier <mort@cantab.net>

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,7 +13,12 @@ if [ ! -r "$HOME/.abuild/abuild.conf" ]; then
   abuild-keygen -n -a
 fi
 
-( . "$HOME/.abuild/abuild.conf" ; if [ ! -s "$PACKAGER_PRIVKEY" ]; then abuild-keygen -n -a; fi )
+(
+  . "$HOME/.abuild/abuild.conf"
+  if [ ! -s "$PACKAGER_PRIVKEY" ]; then
+    abuild-keygen -n -a
+  fi
+)
 
 sudo cp -v "$HOME"/.abuild/*.rsa.pub /etc/apk/keys/
 sudo apk -U upgrade -a
